1c375adae6f71e6647985e84ac3184a7308b7d2b
Renan Araújo <awkrenan@gmail.com>
2024-06-12 08:25:45 -0300

    :)

---
+++ books/SICP/exercises/1.10.scm
@@ -0,0 +1,58 @@
+; ackermann function
+(define (ack m n)
+  (cond ((= n 0) 0)
+        ((= m 0) (* 2 n))
+        ((= n 1) 2)
+        (else (ack (- m 1) (ack m (- n 1))))))
+
+; im definitely not doing all the results, its big af
+; but the first one is this:
+; (ack 1 10)
+
+(A 1 10)
+(A 0 (A 1 9))
+(A 0 (A 0 (A 1 8)))
+(A 0 (A 0 (A 0 (A 1 7))))
+(A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+(A 0 (A 0 (A 0 (A 0 64))))
+(A 0 (A 0 (A 0 128)))
+(A 0 (A 0 256))
+(A 0 512)
+1024
+
+; the others are (ack 2 4) and (ack 3 3), but i aint doing them
+
+; Consider the following procedures, where A is the procedure defined above:
+
+(define (f n) (A 0 n))
+
+(define (g n) (A 1 n))
+
+(define (h n) (A 2 n))
+
+(define (k n) (* 5 n n))
+
+; Give concise mathematical definitions for the functions computed by the 
+; procedures f, g, and h for positive integer values of n. For example, (k n) computes 5n2.
+
+f(n)
+; m is 0, so it return n * 2
+f(n) = 2n
+
+g(n)
+; m is 1, recursive call
+g(n) = 2^n
+
+h(n)
+; m in 2, recursive call
+h(n) = 2^(2^n)
