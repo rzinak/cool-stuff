b2c4f6e962ed8487307d8c9a3b3fdb8241509695
Renan Araujo <awkrenan@gmail.com>
2024-06-22 22:45:50 -0300

    >(

---
+++ books/SICP/exercises/1.13.scm
@@ -0,0 +1,8 @@
+; Prove that Fib(n) is the closest integer to φ^n/√5, where φ = (1 + √5)/2. 
+; Hint: Let 𝛙 = (1 - √5)/2. 
+; Use induction and the definition of the Fibonacci numbers (see section 1.2.2) 
+; to prove that Fib(n) = (φ^n - 𝛙^n)/√5.
+
+gave up on this, i think i need to show that the error margin is small enough to say
+that Fib(n) can get rounded to φ^n/√5, but im stuck and cant progress, fuck this book
+the error term is psi, i suppose... because phi grows exponentially, and psi does the opposite
